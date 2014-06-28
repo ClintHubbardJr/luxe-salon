a9d53636eef3aadbade537fa4da68f02759006cb
chubbard <chubbard@deloitte.com>
2014-06-27 17:43:19 -0700

    kitty layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,12 +3,3 @@
 $('.navbar-brand').click(function () {
   $('.navbar-nav li').removeClass('active');
 });
-
-function parallax() {
-  var scrolled = $(window).scrollTop();
-  $('#call-to-action').css('bottom', -(scrolled * 0.1) + 'px');
-}
-
-$(window).scroll(function (e) {
-  parallax();
-});
