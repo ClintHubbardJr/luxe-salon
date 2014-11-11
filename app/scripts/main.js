a9c3d8574ffe4b28bba3d90fe20249052ea4286f
chubbard <chubbard@deloitte.com>
2014-11-10 22:04:31 -0800

    Fixed header and banner. Added new image to footer and changed layout.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,3 +3,15 @@
 $('.navbar-brand').click(function () {
   $('.navbar-nav li').removeClass('active');
 });
+
+
+
+//Dynamically assign height
+function sizeHeader() {
+  var headerHeight = $('.welcome').innerHeight() + 'px';
+  $('#call-to-action').css('height', headerHeight);
+}
+
+// $(document).ready(sizeHeader);
+
+$(window).resize(sizeHeader);
