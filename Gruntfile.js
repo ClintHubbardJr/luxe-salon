7d0d0174f71fb2a28baca3d367bade80bfc57080
chubbard <chubbard@deloitte.com>
2017-09-20 12:50:10 -0700

    Updated pricing, services, and product lines.

--- Gruntfile.js
+++ Gruntfile.js
@@ -203,7 +203,7 @@ module.exports = function (grunt) {
     htmlmin: {
       dist: {
         options: {
-          /*removeCommentsFromCDATA: true,
+          removeCommentsFromCDATA: true,
           // https://github.com/yeoman/grunt-usemin/issues/44
           //collapseWhitespace: true,
           collapseBooleanAttributes: true,
@@ -211,7 +211,7 @@ module.exports = function (grunt) {
           removeRedundantAttributes: true,
           useShortDoctype: true,
           removeEmptyAttributes: true,
-          removeOptionalTags: true*/
+          removeOptionalTags: true
         },
         files: [{
           expand: true,
